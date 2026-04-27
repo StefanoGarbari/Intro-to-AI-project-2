3c4b511b5d2c6842d059522aa8eeeff351b932cc
StefanoGarbari <stefano.garbari3@gmail.com>
2026-04-27 16:30:08 +0200

    Add CNF specific data structures

--- belief_revision.py
+++ belief_revision.py
@@ -53,6 +53,63 @@ class BiImplication(Formula):
         return f"({self.left} ↔ {self.right})"
 
 
+@dataclass(frozen=True)
+class Literal:
+    name: str
+    negated: bool = False
+
+    def __str__(self):
+        return f"¬{self.name}" if self.negated else self.name
+
+@dataclass(frozen=True)
+class Clause:
+    literals: frozenset[Literal]
+
+    def __str__(self):
+        if not self.literals:
+            return "⊥"
+        return " ∨ ".join(map(str, self.literals))
+
+@dataclass(frozen=True)
+class CNF:
+    clauses: frozenset[Clause]
+
+    def __str__(self):
+        if not self.clauses:
+            return "⊤"
+        return "(" + ") ∧ (".join(map(str, self.clauses)) + ")"
+
+
+def to_literal(f: Formula) -> Literal:
+    match f:
+        case Proposition(name):
+            return Literal(name, False)
+
+        case Negation(Proposition(name)):
+            return Literal(name, True)
+
+        case _:
+            raise ValueError(f"Not a literal: {f}")
+        
+def to_clause(f: Formula) -> Clause:
+    match f:
+        case Disjunction(a, b):
+            return Clause(
+                to_clause(a).literals.union(to_clause(b).literals)
+            )
+
+        case _:
+            return Clause(frozenset({to_literal(f)}))
+
+def clauses_of(f: Formula) -> list[Clause]:
+    match f:
+        case Conjunction(a, b):
+            return clauses_of(a) + clauses_of(b)
+
+        case _:
+            return [to_clause(f)]
+
+
 def eliminate_implications(f: Formula) -> Formula:
     """
     returns a formula equivalent to f, without implications or bi-implications
@@ -131,15 +188,16 @@ def distribute_to_cnf(f: Formula) -> Formula:
         
         # Implications, bi-implications and negations are handled by the steps before
 
-def to_cnf(formula: Formula) -> Formula:
+def to_cnf(formula: Formula) -> CNF:
     f = eliminate_implications(formula)
     f = to_nnf(f)
     f = distribute_to_cnf(f)
-    return f
 
+    clauses = clauses_of(f)
+    return CNF(frozenset(clauses))
 
 # tests
-f = BiImplication("r", Disjunction("p", "s"))
+f = BiImplication(Proposition("r"), Disjunction(Proposition("p"), Proposition("s")))
 e = to_cnf(f)
 print(f)
 print(e)
