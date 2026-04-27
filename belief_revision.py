334fd840881ba7a31b2977428ef0fa86a8f4df2c
StefanoGarbari <stefano.garbari3@gmail.com>
2026-04-27 15:18:42 +0200

    Add CNF conversion

--- belief_revision.py
+++ belief_revision.py
@@ -53,10 +53,94 @@ class BiImplication(Formula):
         return f"({self.left} ↔ {self.right})"
 
 
+def eliminate_implications(f: Formula) -> Formula:
+    """
+    returns a formula equivalent to f, without implications or bi-implications
+    """
+    match f:
+        case Implication(a, b):
+            return Disjunction(Negation(eliminate_implications(a)), eliminate_implications(b))
+
+        case BiImplication(a, b):
+            return Conjunction(eliminate_implications(Implication(a, b)), eliminate_implications(Implication(b, a)))
+
+        case Negation(x):
+            return Negation(eliminate_implications(x))
+
+        case Conjunction(a, b):
+            return Conjunction(eliminate_implications(a), eliminate_implications(b))
+
+        case Disjunction(a, b):
+            return Disjunction(eliminate_implications(a), eliminate_implications(b))
+
+        case _:
+            return f
+
+def to_nnf(f: Formula) -> Formula:
+    """
+    returns a formula equivalent to f, after pushing negations inward
+    """
+    match f:
+        case Negation(Negation(x)):
+            return to_nnf(x)
+
+        case Negation(Conjunction(a, b)): # de morgan
+            return Disjunction(to_nnf(Negation(a)), to_nnf(Negation(b)))
+
+        case Negation(Disjunction(a, b)): # de morgan
+            return Conjunction(to_nnf(Negation(a)), to_nnf(Negation(b)))
+
+        case Conjunction(a, b):
+            return Conjunction(to_nnf(a), to_nnf(b))
+
+        case Disjunction(a, b):
+            return Disjunction(to_nnf(a), to_nnf(b))
+
+        case Negation(x):
+            return Negation(to_nnf(x))
+
+        case _:
+            return f
+
+def distribute_or(a: Formula, b: Formula) -> Formula:
+    """
+    returns a formula equivalent to f, after distributing ∧ over ∨.
+    e.g. (A ∨ (B ∧ C)) → (A ∨ B) ∧ (A ∨ C)
+    """
+    match (a, b):
+
+        case (Conjunction(a1, a2), _):
+            return Conjunction(distribute_or(a1, b), distribute_or(a2, b))
+
+        case (_, Conjunction(b1, b2)):
+            return Conjunction(distribute_or(a, b1), distribute_or(a, b2))
+
+        case _:
+            return Disjunction(a, b)
+
+def distribute_to_cnf(f: Formula) -> Formula:
+    match f:
+        case Conjunction(a, b):
+            return Conjunction(distribute_to_cnf(a), distribute_to_cnf(b))
+
+        case Disjunction(a, b):
+            return distribute_or(distribute_to_cnf(a), distribute_to_cnf(b))
+
+        case _:
+            return f
+        
+        # Implications, bi-implications and negations are handled by the steps before
+
+def to_cnf(formula: Formula) -> Formula:
+    f = eliminate_implications(formula)
+    f = to_nnf(f)
+    f = distribute_to_cnf(f)
+    return f
+
 
 # tests
-f = BiImplication(Negation(Disjunction("a", "b")), "c")
-e = Negation(Negation("a"))
+f = BiImplication("r", Disjunction("p", "s"))
+e = to_cnf(f)
 print(f)
 print(e)
 print(f == e)
